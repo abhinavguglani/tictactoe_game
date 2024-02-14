0ba0f41a9482d002cf6435ef664953a277594273
abhinavguglani <77040136+abhinavguglani@users.noreply.github.com>
2024-02-14 21:45:07 +0530

    Added 1 fix

--- app.js
+++ app.js
@@ -77,11 +77,12 @@ const checkWinner = () => {
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if (pos1Val != "" && pos2Val != "" && pos3Val != "")
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 showWinner(pos1Val)
                 return true
             }
+        }
     }
 }
 
